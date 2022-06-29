72a5bdc33addb108ea62afd08872b690afbdc6e9
prip <kuzin.olm@gmail.com>
2022-06-29 19:58:53 +0500

    fix: open image with context manager

--- services/image.py
+++ services/image.py
@@ -2,23 +2,22 @@ from PIL import Image
 
 
 def resize_img(image_path, max_width=1280, max_height=1280):
-    img = Image.open(image_path)
-    _width, _height = img.size
+    with Image.open(image_path) as img:
+        _width, _height = img.size
 
-    is_resizing = False
-    if _width > max_width:
-        w = _width / max_width
-        _width /= w
-        _height /= w
-        is_resizing = True
+        is_resizing = False
+        if _width > max_width:
+            w = _width / max_width
+            _width /= w
+            _height /= w
+            is_resizing = True
 
-    if _height > max_height:
-        h = _height / max_height
-        _height /= h
-        _width /= h
-        is_resizing = True
+        if _height > max_height:
+            h = _height / max_height
+            _height /= h
+            _width /= h
+            is_resizing = True
 
-    if is_resizing:
-        img.thumbnail((_width, _height))
-        img.save(image_path)
-        img.close()
+        if is_resizing:
+            img.thumbnail((_width, _height))
+            img.save(image_path)
